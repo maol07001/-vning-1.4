2a7513f50ccae8aac7eeac137b521158dd553e64
maol07001 <maol07001@E-PW05CGR4>
2024-09-12 13:49:08 +0200

    Add project files.

---
+++ övning 1.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift_1._4
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+            Console.WriteLine("detta ser lite ut som den svenska flaggan");
+
+        }
+    }
+}
